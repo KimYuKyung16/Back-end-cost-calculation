3ddbda906b2cae9c1add41d48f5da47804351e7a
kyk <kyg0954@naver.com>
2023-07-14 22:25:45 +0900

    Refactor: app 정리, 이전에 올려놓은 프로필 이미지 삭제

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,16 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 const authRoute = require("./auth");
+const userRoute = require("./user");
 const friendRoute = require("./friend"); 
-const appointmentRoute = require("./appointment"); 
+const appointmentRoute = require("./calculate"); 
 const costRoute = require("./cost"); 
-
+const messageRoute = require("./message"); 
 
 
 router.use("/auth", authRoute); // 로그인 관련 라우터
+router.use("/user", userRoute); // 유저 관련 라우터
 router.use("/friend", friendRoute); // 친구 관련 라우터
-router.use("/appointment", appointmentRoute); // 일정 관련 라우터
+router.use("/calculate", appointmentRoute); // 정산 관련 라우터
 router.use("/cost", costRoute); // 비용 관련 라우터
+router.use("/message", messageRoute); // 쪽지 관련 라우터
 
 
 module.exports = router // 모듈로 리턴
